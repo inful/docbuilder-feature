a37aef75eea83f488faf712ffa24ef1256b7d087
Jone Marius Vignes <jvig@ihelse.net>
2026-01-02 10:34:08 +0100

    Add debug output to wrapper script to see available env vars

--- features/docbuilder/devcontainer-features-install.sh
+++ features/docbuilder/devcontainer-features-install.sh
@@ -4,6 +4,12 @@
 
 set -e
 
+# Debug: Print all environment variables to see what's available
+echo "DEBUG: Available environment variables in wrapper:"
+env | grep -i proxy || echo "No proxy variables found"
+env | grep -i docbuilder || echo "No docbuilder variables found"
+env | grep -i hugo || echo "No hugo variables found"
+
 # Feature options are passed as environment variables
 # devcontainers CLI converts them to OPTION_NAME format
 # But we need to make sure they're available to the install script
